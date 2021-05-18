47c7f1741cf2371e9e342b9f75b96253b6624218
louisenje <louisenje@gmail.com>
2021-05-18 07:34:05 +0300

    added search and image urls and paths

--- images/urls.py
+++ images/urls.py
@@ -5,6 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.index,name='index'),
+    url(r'^search/',views.search_results,name='search_results'),
+    url(r'^image/(?P<image_id>(\d+))',views.imagepath,name='imagepath'),
     
 ]
 if settings.DEBUG:
