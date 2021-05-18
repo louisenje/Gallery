7547db1d51608a9313a50a44ca9fdfcef2e74ad0
louisenje <louisenje@gmail.com>
2021-05-18 07:21:53 +0300

    registered my models in admin

--- images/admin.py
+++ images/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import image,location,category
 # Register your models here.
+admin.site.register(image)
+admin.site.register(location)
+admin.site.register(category)
